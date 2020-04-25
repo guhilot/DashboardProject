5be0f3e4aa296152bdc2d961ab78048a664d157e
vikramguhilot <guhilot@pdx.edu>
2020-04-25 16:46:13 -0700

    Updates

---
+++ precache-manifest.12260d066b595ab8825d34c1c5c9ec5e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbddc43ace53b0aa664f7b79e52d6ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82edf20474fb825ec13",
+    "url": "/static/css/main.8d160792.chunk.css"
+  },
+  {
+    "revision": "1dbef6a06581d3a58dca",
+    "url": "/static/js/2.0ba4595f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ba4595f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82edf20474fb825ec13",
+    "url": "/static/js/main.5b8b001a.chunk.js"
+  },
+  {
+    "revision": "6cb30fca1038e71bdbf7",
+    "url": "/static/js/runtime-main.877efe94.js"
+  },
+  {
+    "revision": "be841c07dafc59dfb974b1f209284657",
+    "url": "/static/media/gold.be841c07.jpg"
+  },
+  {
+    "revision": "d8d0e42a2553b3b5ec21756e4fe5afa1",
+    "url": "/static/media/palla.d8d0e42a.jpg"
+  },
+  {
+    "revision": "3a6823089c8c79340561fc629f173ff1",
+    "url": "/static/media/platinum.3a682308.jpg"
+  },
+  {
+    "revision": "4fd9c9e33486d8e36ae40d877dc5eb0a",
+    "url": "/static/media/silver.4fd9c9e3.jpg"
+  }
+]);
